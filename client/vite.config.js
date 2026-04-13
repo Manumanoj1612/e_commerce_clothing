abfe322d04b7f82dd5c1884f2c44817280ad6868
Manumanoj1612 <manumanoj1611@gmail.com>
2026-04-13 15:27:17 +0530

    origin

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://e-commerce-clothing-rizl.onrender.com',
         changeOrigin: true,
       }
     }
